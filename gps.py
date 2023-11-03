e111a5880796a492f6dd3e0adc2f79cd4cf8cf18
Nikhilesh <nikhileshs19@gmail.com>
2023-11-03 18:53:44 +0530

    gps try

--- gps.py
+++ gps.py
@@ -25,20 +25,47 @@
 #     sock.close()
 #     ser.close()
 
-
 import socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-HOST = "10.0.0.11"
-PORT = 5005
-server_address = (HOST, PORT)
-print('Listening on', server_address)
+from flask_socketio import SocketIO
+from flask import Flask
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+# Specify the IP and port for the UDP server
+UDP_HOST = "10.0.0.11"
+UDP_PORT = 5005
+
+# Specify the IP and port for the Flask app
+FLASK_HOST = "192.168.42.172"
+FLASK_PORT = 7777
+
+# Bind the UDP server
+udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_sock.bind((UDP_HOST, UDP_PORT))
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
 try:
-    sock.bind(server_address)
     while True:
-        received_data, sender = sock.recvfrom(1024)
+        received_data, sender = udp_sock.recvfrom(1024)
         received_data = received_data.decode()
         print("Received data from {}: {}".format(sender, received_data))
+
+        # Send the received data to the Flask app
+        socketio.emit('gps_data', received_data)
+
 except OSError as e:
     print(f"Error: {e}")
+
 finally:
-    sock.close()
+    udp_sock.close()
+
+if __name__ == '__main__':
+    socketio.run(app, host=FLASK_HOST, port=FLASK_PORT, debug=False)
